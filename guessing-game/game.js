08a16909b83cdeaaa9a57ee0a2f1011b876be260
Loretta Akorfa <bemponglaureta4@gmail.com>
2025-09-23 09:04:51 -0700

    Day 3: Player can keep guessing until correct(Upgrade).

--- guessing-game/game.js
+++ guessing-game/game.js
@@ -5,12 +5,18 @@ const rl = readline.createInterface({
    output:process.stdout
 });
 const secretNumber = Math.floor(Math.random() *10) + 1;
+let attempts = 0;
+function askGuess() {
 rl.question("Guess a number between 1 and 10:",(answer) => {
   const guess = parseInt(answer);
+  attempts++;
   if (guess === secretNumber) {
-    console.log("Correct!You guessed the Number!");
+    console.log(`Correct!You guessed the Number in ${attempts}tries.`);
+    rl.close();
   }else{
-    console.log(`Wrong!The secret number was ${secretNumber}.`);
+    console.log("Wrong!Try again");
+    askGuess();
   }
- rl.close();
 });
+}
+askGuess();
